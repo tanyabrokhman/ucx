09beb91f890f8aa9c907dc266f2ad36df18aa758
Shuki Zanyovka <shuki.zanyovka@huawei.com>
2020-03-31 12:15:37 +0300

    UCS: Improve foreach() performance of ptr-array and add locked interface
    - Improve foreach() performance on sparse ptr-arrays.
    - Instead,
      - Removed the placeholder field completely.
      - Update all ptr_array tests not to check place_holder value.
      - Use the field of placeholder as size_free_ahead - I.e. an indicator
        to how many free elements are ahead.
      - Easily implemented at O(1) via looking at the next element,
        index=i+1, and assigning the new removed
        element the next element's size_free value the following:
        - new_removed element's size_free = next element's free size + 1
        - If the next element is not free ==> size_free = 1
    - Now we see an increase in speed of ~30-35% in foreach while not affecting
      remove() / insert() or any other operation significantly:
      old code foreach time for element = ~1.46 nsec
      new code foreach time for element = ~1.02 nsec
    - Adds test for foreach in gtest.
    - Does not affect insert / remove performance.
    - Introduce a new type: ucs_ptr_array_locked_t, based on a
      ucs_recursive_spinlock_t.
      - Provide interface fuctions for it, ucs_ptr_array_locked_*().
      - Add test in test_datatype.cc

--- src/ucs/datastruct/ptr_array.c
+++ src/ucs/datastruct/ptr_array.c
@@ -20,23 +20,18 @@
 #define UCS_PTR_ARRAY_INITIAL_SIZE  8
 
 
-static inline int ucs_ptr_array_is_free(ucs_ptr_array_t *ptr_array, unsigned idx)
+static inline int
+ucs_ptr_array_is_free(ucs_ptr_array_t *ptr_array, unsigned element_index)
 {
-    return (idx < ptr_array->size) &&
-            __ucs_ptr_array_is_free(ptr_array->start[idx]);
+    return (element_index < ptr_array->size) &&
+            __ucs_ptr_array_is_free(ptr_array->start[element_index]);
 }
 
-static inline uint32_t ucs_ptr_array_placeholder_get(ucs_ptr_array_elem_t elem)
+static inline uint32_t
+ucs_ptr_array_size_free_get(ucs_ptr_array_elem_t elem)
 {
     ucs_assert(__ucs_ptr_array_is_free(elem));
-    return elem >> UCS_PTR_ARRAY_PLCHDR_SHIFT;
-}
-
-static inline void ucs_ptr_array_placeholder_set(ucs_ptr_array_elem_t *elem,
-                                                 uint32_t placeholder)
-{
-    *elem = (*elem & ~UCS_PTR_ARRAY_PLCHDR_MASK) |
-                    (((ucs_ptr_array_elem_t)placeholder) << UCS_PTR_ARRAY_PLCHDR_SHIFT);
+    return elem >> UCS_PTR_ARRAY_SIZE_FREE_SHIFT;
 }
 
 static inline unsigned
@@ -54,16 +49,39 @@ ucs_ptr_array_freelist_set_next(ucs_ptr_array_elem_t *elem, unsigned next)
                     (((ucs_ptr_array_elem_t)next) << UCS_PTR_ARRAY_NEXT_SHIFT);
 }
 
+static inline unsigned
+ucs_ptr_array_freelist_get_next_params(ucs_ptr_array_elem_t elem,
+                                       uint32_t *size_free)
+{
+    ucs_assert(__ucs_ptr_array_is_free(elem));
+    *size_free = elem >> UCS_PTR_ARRAY_SIZE_FREE_SHIFT;
+    return ((elem & UCS_PTR_ARRAY_NEXT_MASK) >> UCS_PTR_ARRAY_NEXT_SHIFT);
+}
+
+static inline void
+ucs_ptr_array_freelist_element_set_free(ucs_ptr_array_elem_t *elem,
+                                        uint32_t size_free,
+	                                unsigned next)
+{
+    ucs_assert(next <= UCS_PTR_ARRAY_NEXT_MASK);
+
+    *elem = UCS_PTR_ARRAY_FLAG_FREE |
+            (((ucs_ptr_array_elem_t)size_free) << UCS_PTR_ARRAY_SIZE_FREE_SHIFT) |
+            (((ucs_ptr_array_elem_t)next) << UCS_PTR_ARRAY_NEXT_SHIFT);
+}
+
 static void UCS_F_MAYBE_UNUSED ucs_ptr_array_dump(ucs_ptr_array_t *ptr_array)
 {
 #if UCS_ENABLE_ASSERT
     unsigned i;
 
-    ucs_trace_data("ptr_array start %p size %u", ptr_array->start, ptr_array->size);
+    ucs_trace_data("ptr_array start %p size %u",
+                   ptr_array->start, ptr_array->size);
     for (i = 0; i < ptr_array->size; ++i) {
         if (ucs_ptr_array_is_free(ptr_array, i)) {
-            ucs_trace_data("[%u]=<free> (%u)", i,
-                           ucs_ptr_array_placeholder_get(ptr_array->start[i]));
+            ucs_trace_data("(%u) [%u]=<free> [%u]=<next>", i,
+                           ucs_ptr_array_size_free_get(ptr_array->start[i]),
+                           ucs_ptr_array_freelist_get_next(ptr_array->start[i]));
         } else {
             ucs_trace_data("[%u]=%p", i, (void*)ptr_array->start[i]);
         }
@@ -85,10 +103,8 @@ static void ucs_ptr_array_clear(ucs_ptr_array_t *ptr_array)
     ptr_array->freelist         = UCS_PTR_ARRAY_SENTINEL;
 }
 
-void ucs_ptr_array_init(ucs_ptr_array_t *ptr_array, uint32_t init_placeholder,
-                        const char *name)
+void ucs_ptr_array_init(ucs_ptr_array_t *ptr_array, const char *name)
 {
-    ptr_array->init_placeholder = init_placeholder;
     ucs_ptr_array_clear(ptr_array);
 #ifdef ENABLE_MEMTRACK
     ucs_snprintf_zero(ptr_array->name, sizeof(ptr_array->name), "%s", name);
@@ -103,7 +119,8 @@ void ucs_ptr_array_cleanup(ucs_ptr_array_t *ptr_array)
     for (i = 0; i < ptr_array->size; ++i) {
         if (!ucs_ptr_array_is_free(ptr_array, i)) {
             ++inuse;
-            ucs_trace("ptr_array(%p) idx %d is not free during cleanup", ptr_array, i);
+            ucs_trace("ptr_array(%p) idx %d is not free during cleanup",
+                      ptr_array, i);
         }
     }
 
@@ -120,6 +137,7 @@ static void ucs_ptr_array_grow(ucs_ptr_array_t *ptr_array UCS_MEMTRACK_ARG)
     ucs_ptr_array_elem_t *new_array;
     unsigned curr_size, new_size;
     unsigned i, next;
+    unsigned size_free_ahead;
 
     curr_size = ptr_array->size;
     if (curr_size == 0) {
@@ -134,10 +152,10 @@ static void ucs_ptr_array_grow(ucs_ptr_array_t *ptr_array UCS_MEMTRACK_ARG)
     memcpy(new_array, ptr_array->start, curr_size * sizeof(ucs_ptr_array_elem_t));
 
     /* Link all new array items */
+    size_free_ahead = new_size - curr_size;
     for (i = curr_size; i < new_size; ++i) {
-        new_array[i] = UCS_PTR_ARRAY_FLAG_FREE;
-        ucs_ptr_array_placeholder_set(&new_array[i], ptr_array->init_placeholder);
-        ucs_ptr_array_freelist_set_next(&new_array[i], i + 1);
+        ucs_ptr_array_freelist_element_set_free(&new_array[i], size_free_ahead--,
+                                                i + 1);
     }
     ucs_ptr_array_freelist_set_next(&new_array[new_size - 1], UCS_PTR_ARRAY_SENTINEL);
 
@@ -159,11 +177,11 @@ static void ucs_ptr_array_grow(ucs_ptr_array_t *ptr_array UCS_MEMTRACK_ARG)
     ptr_array->size  = new_size;
 }
 
-unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value,
-                              uint32_t *placeholder_p)
+unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value)
 {
     ucs_ptr_array_elem_t *elem;
-    unsigned elem_index;
+    unsigned element_index;
+    uint32_t size_free_ahead;
 
     ucs_assert_always(((uintptr_t)value & UCS_PTR_ARRAY_FLAG_FREE) == 0);
 
@@ -172,37 +190,128 @@ unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value,
     }
 
     /* Get the first item on the free list */
-    elem_index = ptr_array->freelist;
-    ucs_assert(elem_index != UCS_PTR_ARRAY_SENTINEL);
-    elem = &ptr_array->start[elem_index];
+    element_index = ptr_array->freelist;
+    ucs_assert(element_index != UCS_PTR_ARRAY_SENTINEL);
 
-    /* Remove from free list */
-    ptr_array->freelist = ucs_ptr_array_freelist_get_next(*elem);
+    elem = &ptr_array->start[element_index];
 
-    /* Populate */
-    *placeholder_p = ucs_ptr_array_placeholder_get(*elem);
-    *elem = (uintptr_t)value;
-    return elem_index;
+    /* Remove from free list and populate */
+    ptr_array->freelist =
+        ucs_ptr_array_freelist_get_next_params(*elem, &size_free_ahead);
+    *elem               = (uintptr_t)value;
+
+    return element_index;
 }
 
-void ucs_ptr_array_remove(ucs_ptr_array_t *ptr_array, unsigned elem_index,
-                          uint32_t placeholder)
+void ucs_ptr_array_remove(ucs_ptr_array_t *ptr_array, unsigned element_index)
 {
-    ucs_ptr_array_elem_t *elem = &ptr_array->start[elem_index];
+    ucs_ptr_array_elem_t *elem = &ptr_array->start[element_index];
+    ucs_ptr_array_elem_t *next_elem;
+    uint32_t size_free_ahead;
 
-    ucs_assert_always(!ucs_ptr_array_is_free(ptr_array, elem_index));
-    *elem = UCS_PTR_ARRAY_FLAG_FREE;
-    ucs_ptr_array_placeholder_set(elem, placeholder);
-    ucs_ptr_array_freelist_set_next(elem, ptr_array->freelist);
-    ptr_array->freelist = elem_index;
+    ucs_assert_always(!ucs_ptr_array_is_free(ptr_array, element_index));
+
+    if (((element_index + 1) < ptr_array->size) &&
+        (__ucs_ptr_array_is_free(ptr_array->start[element_index + 1]))) {
+        next_elem = &ptr_array->start[element_index + 1];
+        ucs_ptr_array_freelist_get_next_params(*next_elem, &size_free_ahead);
+        size_free_ahead++;
+    } else {
+        size_free_ahead = 1;
+    }
+
+    ucs_ptr_array_freelist_element_set_free(elem, size_free_ahead,
+                                            ptr_array->freelist);
+
+    /* Make sure the next element is free */
+    ucs_assert(__ucs_ptr_array_is_free(ptr_array->start[element_index + size_free_ahead - 1]));
+
+    ptr_array->freelist = element_index;
 }
 
-void *ucs_ptr_array_replace(ucs_ptr_array_t *ptr_array, unsigned elem_index, void *new_val)
+void *ucs_ptr_array_replace(ucs_ptr_array_t *ptr_array, unsigned element_index,
+                            void *new_val)
 {
     void *old_elem;
 
-    ucs_assert_always(!ucs_ptr_array_is_free(ptr_array, elem_index));
-    old_elem = (void *)ptr_array->start[elem_index];
-    ptr_array->start[elem_index] = (uintptr_t)new_val;
+    ucs_assert_always(!ucs_ptr_array_is_free(ptr_array, element_index));
+    old_elem                = (void *)ptr_array->start[element_index];
+    ptr_array->start[element_index] = (uintptr_t)new_val;
     return old_elem;
 }
+
+
+/*
+ *  Locked interface functions implementation
+ */
+
+ucs_status_t
+ucs_ptr_array_locked_init(ucs_ptr_array_locked_t *locked_ptr_array,
+                          const char *name)
+{
+    ucs_status_t status;
+
+    /* Initialize spinlock */
+    status = ucs_recursive_spinlock_init(&locked_ptr_array->lock, 0);
+    if (status != UCS_OK) {
+       return status;
+    }
+
+    /* Call unlocked function */
+    ucs_ptr_array_init(&locked_ptr_array->super, name);
+
+    return UCS_OK;
+}
+
+void ucs_ptr_array_locked_cleanup(ucs_ptr_array_locked_t *locked_ptr_array)
+{
+    ucs_status_t status;
+
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    ucs_ptr_array_cleanup(&locked_ptr_array->super);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+
+    /* Destroy spinlock */
+    status = ucs_recursive_spinlock_destroy(&locked_ptr_array->lock);
+    if (status != UCS_OK) {
+        ucs_warn("ucs_recursive_spinlock_destroy() - failed (%d)", status);
+    }
+}
+
+unsigned ucs_ptr_array_locked_insert(ucs_ptr_array_locked_t *locked_ptr_array,
+                                     void *value)
+{
+    unsigned element_index;
+
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    element_index = ucs_ptr_array_insert(&locked_ptr_array->super, value);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+
+    return element_index;
+}
+
+void ucs_ptr_array_locked_remove(ucs_ptr_array_locked_t *locked_ptr_array,
+                                 unsigned element_index)
+{
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    ucs_ptr_array_remove(&locked_ptr_array->super, element_index);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+}
+
+void *ucs_ptr_array_locked_replace(ucs_ptr_array_locked_t *locked_ptr_array,
+                                   unsigned element_index, void *new_val)
+{
+    void *old_elem;
+
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    old_elem = ucs_ptr_array_replace(&locked_ptr_array->super, element_index,
+                                     new_val);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+
+    return old_elem;
+}
+
